84fe1ce8e2881e46f8aaaae901c98b0775d3bb71
ElonaKajtazi <elona.kajtazi@gmail.com>
2022-06-15 17:08:37 +0200

    Final

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // - Ask the user for their age and how far they are travelling (in km)
 
-// let userAge = prompt("How old are you?");
-// let userDistance = prompt("How far are you travelling?");
+// let userAge = Number(prompt("How old are you?"));
+// let userDistance = Number(prompt("How far are you travelling?"));
 
 //    -- The price per travelled km will be £0.21
 // let pricePerKm = 0.21;
@@ -33,9 +33,11 @@ Try also to code it so that you only need to change the price per km in one plac
 let pricePerKm = 0.21;
 
 //Starting Point
-let startingPoint = prompt("Where are you starting from?");
+let startingPoint = prompt(`Where are you starting from?
+Choose from: Sishman, Babaj, Duzhnje, Korenice, Meje, Gjakova, Prishtina`);
 // Destination
-let destination = prompt("Where are you going?");
+let destination = prompt(`Where are you going? 
+Choose from: Sishman, Babaj, Duzhnje, Korenice, Meje, Gjakova, Prishtina`);
 // Station Names
 let station1 = "Shishman";
 let station2 = "Babaj";
@@ -53,276 +55,289 @@ let station1ToStation6Distance = 30;
 let station1ToStation7Distance = 110;
 
 // STARTING POINT: STATION1 - SHISHMAN
-if (startingPoint === station1 && destination === station2) {
-  console.log(
-    `You need to pay £${(pricePerKm * station1ToStation2Distance).toFixed(2)}`
-  );
-} else if (startingPoint === station1 && destination === station3) {
-  console.log(
-    `You need to pay £${(pricePerKm * station1ToStation3Distance).toFixed(2)}`
-  );
-} else if (startingPoint === station1 && destination === station4) {
-  console.log(
-    `You need to pay £${(pricePerKm * station1ToStation4Distance).toFixed(2)}`
-  );
-} else if (startingPoint === station1 && destination === station5) {
-  console.log(
-    `You need to pay £${(pricePerKm * station1ToStation5Distance).toFixed(2)}`
-  );
-} else if (startingPoint === station1 && destination === station6) {
-  console.log(
-    `You need to pay £${(pricePerKm * station1ToStation6Distance).toFixed(2)}`
-  );
-} else if (startingPoint === station1 && destination === station7) {
-  console.log(
-    `You need to pay £${(pricePerKm * station1ToStation7Distance).toFixed(2)}`
-  );
+if (startingPoint === station1) {
+  if (destination === station2) {
+    console.log(
+      `You need to pay £${(pricePerKm * station1ToStation2Distance).toFixed(2)}`
+    );
+  } else if (destination === station3) {
+    console.log(
+      `You need to pay £${(pricePerKm * station1ToStation3Distance).toFixed(2)}`
+    );
+  } else if (destination === station4) {
+    console.log(
+      `You need to pay £${(pricePerKm * station1ToStation4Distance).toFixed(2)}`
+    );
+  } else if (destination === station5) {
+    console.log(
+      `You need to pay £${(pricePerKm * station1ToStation5Distance).toFixed(2)}`
+    );
+  } else if (destination === station6) {
+    console.log(
+      `You need to pay £${(pricePerKm * station1ToStation6Distance).toFixed(2)}`
+    );
+  } else if (destination === station7) {
+    console.log(
+      `You need to pay £${(pricePerKm * station1ToStation7Distance).toFixed(2)}`
+    );
+  }
 }
-
 //SARTING POINT: STATION2 - BABAJ
-if (startingPoint === station2 && destination === station1) {
-  console.log(
-    `You need to pay £${(pricePerKm * station1ToStation2Distance).toFixed(2)}`
-  );
-} else if (startingPoint === station2 && destination === station3) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation3Distance - station1ToStation2Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station2 && destination === station4) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation4Distance - station1ToStation2Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station2 && destination === station5) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation5Distance - station1ToStation2Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station2 && destination === station6) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation6Distance - station1ToStation2Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station2 && destination === station7) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation7Distance - station1ToStation2Distance)
-    ).toFixed(2)}`
-  );
+if (startingPoint === station2) {
+  if (destination === station1) {
+    console.log(
+      `You need to pay £${(pricePerKm * station1ToStation2Distance).toFixed(2)}`
+    );
+  } else if (destination === station3) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation3Distance - station1ToStation2Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station4) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation4Distance - station1ToStation2Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station5) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation5Distance - station1ToStation2Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station6) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation6Distance - station1ToStation2Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station7) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation7Distance - station1ToStation2Distance)
+      ).toFixed(2)}`
+    );
+  }
 }
 //SARTING POINT: STATION3 - DUZHNJE
-if (startingPoint === station3 && destination === station1) {
-  console.log(
-    `You need to pay £${(pricePerKm * station1ToStation3Distance).toFixed(2)}`
-  );
-} else if (startingPoint === station3 && destination === station2) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation3Distance - station1ToStation2Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station3 && destination === station4) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation4Distance - station1ToStation3Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station3 && destination === station5) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation5Distance - station1ToStation3Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station3 && destination === station6) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation6Distance - station1ToStation3Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station3 && destination === station7) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation7Distance - station1ToStation3Distance)
-    ).toFixed(2)}`
-  );
+if (startingPoint === station3) {
+  if (destination === station1) {
+    console.log(
+      `You need to pay £${(pricePerKm * station1ToStation3Distance).toFixed(2)}`
+    );
+  } else if (destination === station2) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation3Distance - station1ToStation2Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station4) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation4Distance - station1ToStation3Distance)
+      ).toFixed(2)}`
+    );
+  } else if (startingPoint === station3 && destination === station5) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation5Distance - station1ToStation3Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station6) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation6Distance - station1ToStation3Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station7) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation7Distance - station1ToStation3Distance)
+      ).toFixed(2)}`
+    );
+  }
 }
 //SARTING POINT: STATION4 - KORENICE
-if (startingPoint === station4 && destination === station1) {
-  console.log(
-    `You need to pay £${(pricePerKm * station1ToStation4Distance).toFixed(2)}`
-  );
-} else if (startingPoint === station4 && destination === station2) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation4Distance - station1ToStation2Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station4 && destination === station3) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation4Distance - station1ToStation3Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station4 && destination === station5) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation5Distance - station1ToStation4Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station4 && destination === station6) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation6Distance - station1ToStation4Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station4 && destination === station7) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation7Distance - station1ToStation4Distance)
-    ).toFixed(2)}`
-  );
+if (startingPoint === station4) {
+  if (destination === station1) {
+    console.log(
+      `You need to pay £${(pricePerKm * station1ToStation4Distance).toFixed(2)}`
+    );
+  } else if (destination === station2) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation4Distance - station1ToStation2Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station3) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation4Distance - station1ToStation3Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station5) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation5Distance - station1ToStation4Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station6) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation6Distance - station1ToStation4Distance)
+      ).toFixed(2)}`
+    );
+  } else if (startingPoint === station4 && destination === station7) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation7Distance - station1ToStation4Distance)
+      ).toFixed(2)}`
+    );
+  }
 }
 //SARTING POINT: STATION5 - MEJE
-if (startingPoint === station5 && destination === station1) {
-  console.log(
-    `You need to pay £${(pricePerKm * station1ToStation5Distance).toFixed(2)}`
-  );
-} else if (startingPoint === station5 && destination === station2) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation5Distance - station1ToStation2Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station5 && destination === station3) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation5Distance - station1ToStation3Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station5 && destination === station4) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation5Distance - station1ToStation4Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station5 && destination === station6) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation6Distance - station1ToStation5Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station5 && destination === station7) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation7Distance - station1ToStation5Distance)
-    ).toFixed(2)}`
-  );
+if (startingPoint === station5) {
+  if (destination === station1) {
+    console.log(
+      `You need to pay £${(pricePerKm * station1ToStation5Distance).toFixed(2)}`
+    );
+  } else if (destination === station2) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation5Distance - station1ToStation2Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station3) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation5Distance - station1ToStation3Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station4) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation5Distance - station1ToStation4Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station6) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation6Distance - station1ToStation5Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station7) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation7Distance - station1ToStation5Distance)
+      ).toFixed(2)}`
+    );
+  }
 }
 
 //SARTING POINT: STATION6 - GJAKOVA
-if (startingPoint === station6 && destination === station1) {
-  console.log(
-    `You need to pay £${(pricePerKm * station1ToStation6Distance).toFixed(2)}`
-  );
-} else if (startingPoint === station6 && destination === station2) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation6Distance - station1ToStation2Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station6 && destination === station3) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation6Distance - station1ToStation3Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station6 && destination === station4) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation6Distance - station1ToStation4Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station6 && destination === station5) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation6Distance - station1ToStation5Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station6 && destination === station7) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation7Distance - station1ToStation6Distance)
-    ).toFixed(2)}`
-  );
+if (startingPoint === station6) {
+  if (destination === station1) {
+    console.log(
+      `You need to pay £${(pricePerKm * station1ToStation6Distance).toFixed(2)}`
+    );
+  } else if (destination === station2) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation6Distance - station1ToStation2Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station3) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation6Distance - station1ToStation3Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station4) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation6Distance - station1ToStation4Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station5) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation6Distance - station1ToStation5Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station7) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation7Distance - station1ToStation6Distance)
+      ).toFixed(2)}`
+    );
+  }
 }
 // STARTING POINT:  STATION7 - PRISHTINA
-if (startingPoint === station7 && destination === station1) {
-  console.log(
-    `You need to pay £${(pricePerKm * station1ToStation7Distance).toFixed(2)}`
-  );
-} else if (startingPoint === station7 && destination === station2) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation7Distance - station1ToStation2Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station7 && destination === station3) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation7Distance - station1ToStation3Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station7 && destination === station4) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation7Distance - station1ToStation4Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station7 && destination === station5) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation7Distance - station1ToStation5Distance)
-    ).toFixed(2)}`
-  );
-} else if (startingPoint === station7 && destination === station6) {
-  console.log(
-    `You need to pay £${(
-      pricePerKm *
-      (station1ToStation7Distance - station1ToStation6Distance)
-    ).toFixed(2)}`
-  );
+if (startingPoint === station7) {
+  if (destination === station1) {
+    console.log(
+      `You need to pay £${(pricePerKm * station1ToStation7Distance).toFixed(2)}`
+    );
+  } else if (destination === station2) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation7Distance - station1ToStation2Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station3) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation7Distance - station1ToStation3Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station4) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation7Distance - station1ToStation4Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station5) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation7Distance - station1ToStation5Distance)
+      ).toFixed(2)}`
+    );
+  } else if (destination === station6) {
+    console.log(
+      `You need to pay £${(
+        pricePerKm *
+        (station1ToStation7Distance - station1ToStation6Distance)
+      ).toFixed(2)}`
+    );
+  }
 }
